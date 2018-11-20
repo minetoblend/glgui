1aa0400ee69844e876d1933c74bcbdf053d70923
minetoblend <minetoblend@gmail.com>
2018-11-20 03:39:01 +0000

    Added basic transform to Element

--- src/main/java/com/minetoblend/gui/element/Element.java
+++ src/main/java/com/minetoblend/gui/element/Element.java
@@ -1,4 +1,18 @@
 package com.minetoblend.gui.element;
 
+import com.minetoblend.gui.types.ObservableProperty;
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+
 public class Element {
+
+    private final ObservableProperty<Vector2f> position;
+    private final ObservableProperty<Vector2f> size;
+
+    Matrix4f elementMatrix = new Matrix4f();
+
+    public Element() {
+        this.position = new ObservableProperty<>(new Vector2f());
+        this.size = new ObservableProperty<>(new Vector2f());
+    }
 }
