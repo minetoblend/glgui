21be1d0730814f717db1f8a13bb1093fe8a96927
minetoblend <minetoblend@gmail.com>
2018-11-20 03:23:07 +0000

    added color Buffer clear to GLRenderer

--- src/main/java/com/minetoblend/gui/render/gl/GLRenderer.java
+++ src/main/java/com/minetoblend/gui/render/gl/GLRenderer.java
@@ -3,7 +3,10 @@ package com.minetoblend.gui.render.gl;
 import com.minetoblend.gui.render.Renderer;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.opengl.GL11C.*;
+
 public class GLRenderer extends Renderer {
+
 
     public GLRenderer() {
 
@@ -12,6 +15,13 @@ public class GLRenderer extends Renderer {
     @Override
     public void init() {
         GL.createCapabilities();
+    }
+
+    @Override
+    public void render() {
+        glClearColor(1, 1, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
     }
 
 }
