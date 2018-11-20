2f0099767573f90db3feb3b220b40588fb3bca0b
minetoblend <minetoblend@gmail.com>
2018-11-20 02:51:11 +0000

    Created Sample App

---
+++ src/main/java/example/SampleApp.java
@@ -0,0 +1,14 @@
+package example;
+
+import com.minetoblend.gui.Window;
+
+public class SampleApp {
+
+    public static void main(String[] args) {
+
+        var window = new Window();
+
+
+    }
+
+}
